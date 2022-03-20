622ce402a6f86adfded6ce96702f0ec7ac002703
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-03-20 19:46:28 +0400

    1
    1

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -26,25 +26,27 @@ public class Main {
         try {
             while (check) {
                 System.out.println("\n........................Меню программы........................");
-                System.out.print("1 - Шифр Вижинера\n" +
-                        "2 - Шифр с гаммированием\n" +
-                        "3 - Шифр перестановки\n" +
-                        "4 - Омофонический шифр\n" +
-                        "5 - Выход программы\n" +
-                        "Для продолжения введите соответствующую цифру: ");
+                System.out.print("""
+                        1 - Шифр Вижинера
+                        2 - Шифр с гаммированием
+                        3 - Шифр перестановки
+                        4 - Омофонический шифр
+                        5 - Выход программы
+                        Для продолжения введите соответствующую цифру:\s""");
                 int choise = scanner.nextInt();
 
                 switch (choise) {
-                    case 1: {
-                        System.out.print("\n........................Шифр Вижинера........................\n" +
-                                "1 - Шифрование\n" +
-                                "2 - Расшифрование\n" +
-                                "3 - Выход\n" +
-                                "Для продолжения введите соответствующую цифру: ");
-                        int viginerChoise = scanner.nextInt();
+                    case 1 -> {
+                        System.out.print("""
+                                \n........................Шифр Вижинера........................
+                                1 - Шифрование
+                                2 - Расшифрование
+                                3 - Выход
+                                Для продолжения введите соответствующую цифру:\s""");
+                        int vintnerChoice = scanner.nextInt();
 
-                        switch (viginerChoise) {
-                            case 1: {
+                        switch (vintnerChoice) {
+                            case 1 -> {
                                 scanner.nextLine();
 
                                 System.out.print("Введите исходное сообщение: ");
@@ -55,9 +57,8 @@ public class Main {
 
                                 System.out.println("Зашифрованное сообщение: " + viginer.Encode(viginer.getOriginalMessage(), viginer.getKey()));
 
-                                break;
                             }
-                            case 2: {
+                            case 2 -> {
                                 scanner.nextLine();
 
                                 System.out.print("Введите зашифрованное сообщение: ");
@@ -68,30 +69,23 @@ public class Main {
 
                                 System.out.println("Расшированное сообщение: " + viginer.Decode(viginer.getEncryptedMessage(), viginer.getKey()));
 
-                                break;
                             }
-
-                            case 3: {
-                                break;
-                            }
-
-                            default: {
-                                break;
+                            default -> {
                             }
                         }
-                        break;
                     }
-                    case 2: {
-                        System.out.print("\n........................Шифр с гаммированием........................\n" +
-                                "1 - Шифрование\n" +
-                                "2 - Расшифрование\n" +
-                                "3 - Выход\n" +
-                                "Для продолжения введите соответствующую цифру: ");
-                        int scalingChoise = scanner.nextInt();
+                    case 2 -> {
+                        System.out.print("""
+                                \n........................Шифр с гаммированием........................
+                                1 - Шифрование
+                                2 - Расшифрование
+                                3 - Выход
+                                Для продолжения введите соответствующую цифру:\s""");
+                        int scalingChoice = scanner.nextInt();
 
-                        switch (scalingChoise) {
-                            case 1: {
-                                String languageCheck = new String();
+                        switch (scalingChoice) {
+                            case 1 -> {
+                                String languageCheck = "";
 
                                 System.out.print("Введите длину ключа: ");
                                 int a = scanner.nextInt();
@@ -116,9 +110,8 @@ public class Main {
 
                                 System.out.println("Зашифрованное сообщение: " + viginer.Encode(message, pseudoRandom.getPseudoRandomKey()));
 
-                                break;
                             }
-                            case 2: {
+                            case 2 -> {
                                 scanner.nextLine();
 
                                 System.out.print("Введите ключ: ");
@@ -129,26 +122,24 @@ public class Main {
 
                                 System.out.println("Расшифрованное сообщение: " + viginer.Decode(message, pseudoRandomKey));
 
-                                break;
                             }
-                            case 3: {
-                                break;
+                            case 3 -> {
+
                             }
                         }
 
-                        break;
                     }
+                    case 3 -> {
+                        System.out.print("""
+                                \n........................Шифр перестановки........................
+                                1 - Шифрование
+                                2 - Расшифрование
+                                3 - Выход
+                                Для продолжения введите соответствующую цифру:\s""");
+                        int permutationChoice = scanner.nextInt();
 
-                    case 3: {
-                        System.out.print("\n........................Шифр перестановки........................\n" +
-                                "1 - Шифрование\n" +
-                                "2 - Расшифрование\n" +
-                                "3 - Выход\n" +
-                                "Для продолжения введите соответствующую цифру: ");
-                        int permutationChoise = scanner.nextInt();
-
-                        switch (permutationChoise) {
-                            case 1: {
+                        switch (permutationChoice) {
+                            case 1 -> {
                                 System.out.print("Введите длину ключа: ");
                                 int numbersLength = scanner.nextInt();
 
@@ -169,9 +160,8 @@ public class Main {
 
                                 System.out.print("Зашифрованное сообщение: " + permutation.Encode(permutation.getOriginalMessage()) + "\n");
 
-                                break;
                             }
-                            case 2: {
+                            case 2 -> {
                                 scanner.nextLine();
 
                                 System.out.print("Введите сообщение для расшифрования: ");
@@ -192,26 +182,16 @@ public class Main {
 
                                 System.out.print("Расшифрованное сообщение: " + permutation.Decode(permutation.getEncryptedMessage()) + "\n");
 
-                                break;
                             }
+                            case 3 -> {
 
-                            case 3 : {
-                                break;
                             }
                         }
 
-                        break;
                     }
-
-                    case 5: {
+                    case 5 -> {
                         check = false;
-                        continue;
                     }
-
-                    default: {
-                        break;
-                    }
-
                 }
 
 
