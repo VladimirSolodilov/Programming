9b857cd3fcf3254204df875476bd22f0b8c32234
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-04-13 14:34:11 +0400

    1
    1

--- MyProjectSite/src/main/java/com/example/myprojectsite/MyProjectSiteApplication.java
+++ MyProjectSite/src/main/java/com/example/myprojectsite/MyProjectSiteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyProjectSiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyProjectSiteApplication.class, args);
     }
-
 }
