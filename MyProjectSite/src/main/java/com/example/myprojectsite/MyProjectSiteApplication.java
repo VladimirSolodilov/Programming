ee86f08fe3afb0eeadc091c97f3475328acca8c9
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-04-13 11:28:07 +0400

    Сайт
    Создание проекта сайта

---
+++ MyProjectSite/src/main/java/com/example/myprojectsite/MyProjectSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprojectsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectSiteApplication.class, args);
+    }
+
+}
---
+++ MyProjectSite/src/test/java/com/example/myprojectsite/MyProjectSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprojectsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
