ff0d56e4ea0dff629e32aa8716840f24f5995923
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-04-20 19:45:54 +0400

    Практическая работа 3
    Реализация основного функционала

---
+++ MathematicalProject/src/main/java/com/example/mathematicalproject/MathematicalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathematicalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathematicalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathematicalProjectApplication.class, args);
+	}
+
+}
