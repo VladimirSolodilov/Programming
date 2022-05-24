d0c54afac37df6933610e834b9a44a7bd2bcd569
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-05-24 10:41:33 +0400

    1

--- Вычислительная математика/Курсовой проект/CourseProject/src/Main.java
+++ Вычислительная математика/Курсовой проект/CourseProject/src/Main.java
@@ -70,7 +70,7 @@ public class Main {
         System.out.println("Решение оформлено в виде таблицы: ");
 
         System.out.println("|N\t\t\t|x\t\t|Function 3\t\t|Adams 3\t\t|Function 4\t\t\t|Adams 4\t\t|Analytic Function");
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             if ((i > 0 && i < 10) || i % 10 == 0) {
                 System.out.println("|" + i + "\t\t\t|" + String.format("%.3f", xValue(h, N)[i]) + "\t|" + String.format("%.5f", difFunctionValue(N, xValue(h, N), h)[i]) + "\t\t|"
                         + String.format("%.5f", Adams3(N, xValue(h, N), h)[i]) + "\t\t|" + String.format("%.5f", difFunctionValue(N, xValue(h, N), h)[i])
