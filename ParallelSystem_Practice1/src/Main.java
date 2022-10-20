6ab7d6fa8866b269b110fb658ebf76c22348e6f8
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-10-20 07:22:35 +0400

    1

--- ParallelSystem_Practice1/src/Main.java
+++ ParallelSystem_Practice1/src/Main.java
@@ -3,44 +3,50 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        System.out.print("Enter taskValue: ");
+        int taskValue = scanner.nextInt();
+
         System.out.println("\nSequential vector: \n");
 
         for (int i = 10; i <= 100000; i = i * 10) {
-            long time1 = System.nanoTime();
+            long time1 = System.nanoTime() / 1000;
 
-            double[] endVectorSequential1 = sequentialFunction(createStartVector(i), 5);
-            long time2 = System.nanoTime() - time1;
+            sequentialFunction(createStartVector(i), 5, taskValue);
+            long time2 = System.nanoTime() / 1000 - time1;
 
-
-            System.out.println("1 thread " + i + " vector = " + time2 + " ns");
+            System.out.println("1 thread " + i + " vector = " + time2 + " us");
         }
 
         System.out.println("\nParallel Vector: ");
         for (int i = 10; i <= 100000; i = i * 10) {
-            long time1 = System.nanoTime();
+            long time1 = System.nanoTime() / 1000;
 
-            double[] endVectorParallel2 = parallelFunction(createStartVector(i), 2, 5);
-            long time2 = System.nanoTime() - time1;
+            parallelFunction(createStartVector(i), 2, 5, 100, taskValue);
+            long time2 = System.nanoTime() / 1000 - time1;
+            System.out.println("\n2 threads " + i + " vector = " + time2 + " us");
 
-            double[] endVectorParallel3 = parallelFunction(createStartVector(i), 3, 5);
-            long time3 = System.nanoTime() - time1 - time2;
+            parallelFunction(createStartVector(i), 3, 5, 100, taskValue);
+            long time3 = System.nanoTime() / 1000 - time1 - time2;
+            System.out.println("3 threads " + i + " vector = " + time3 + " us");
 
-            double[] endVectorParallel4 = parallelFunction(createStartVector(i), 4, 5);
-            long time4 = System.nanoTime() - time1 - time2 - time3;
+            parallelFunction(createStartVector(i), 4, 5, 100, taskValue);
+            long time4 = System.nanoTime() / 1000 - time1 - time2 - time3;
+            System.out.println("4 threads " + i + " vector = " + time4 + " us");
 
-            double[] endVectorParallel5 = parallelFunction(createStartVector(i), 5, 5);
-            long time5 = System.nanoTime() - time1 - time2 -time3 - time4;
+            parallelFunction(createStartVector(i), 5, 5, 100, taskValue);
+            long time5 = System.nanoTime() / 1000 - time1 - time2 -time3 - time4;
+            System.out.println("5 threads " + i + " vector = " + time5 + " us");
 
-            double[] endVectorParallel10 = parallelFunction(createStartVector(i), 10, 5);
-            long time6 = System.nanoTime() - time1 - time2 -time3 - time4 - time5;
-
-            System.out.println("\n2 threads " + i + " vector = " + time2 + " ns");
-            System.out.println("3 threads " + i + " vector = " + time3 + " ns");
-            System.out.println("4 threads " + i + " vector = " + time4 + " ns");
-            System.out.println("5 threads " + i + " vector = " + time5 + " ns");
-            System.out.println("10 threads " + i + " vector = " + time6 + " ns");
+            parallelFunction(createStartVector(i), 10, 5, 100, taskValue);
+            long time6 = System.nanoTime() / 1000 - time1 - time2 -time3 - time4 - time5;
+            System.out.println("10 threads " + i + " vector = " + time6 + " us");
         }
+
+        /*long time1 = System.nanoTime() / 1000;
+        parallelFunction(createStartVector(100), 5, 5, 100, taskValue);
+        long time2 = System.nanoTime() / 1000 - time1;
+        System.out.println("\n5 threads 100 vector = " + time2 + " us");*/
     }
 
     public static double[] createStartVector(int N) {
@@ -49,56 +55,72 @@ public class Main {
         for (int i = 0; i < startVector.length; i++) {
             startVector[i] = i;
         }
-
         return startVector;
     }
 
-    public static double[] sequentialFunction(double[] startVector, int C) {
+    public static void sequentialFunction(double[] startVector, int C, int taskValue) {
         int K = 100;
         double[] endVector = new double[startVector.length];
 
         for (int i = 0; i < endVector.length; i++) {
-            endVector[i] = Math.pow(startVector[i], C);
-        }
-
-        return endVector;
-    }
-
-    public static double[] parallelFunction(double[] startVector, int M, int C) throws InterruptedException {
-        double[] endVector = new double[startVector.length];
-        AtomicInteger tempValue = new AtomicInteger();
-
-        Runnable runnable = () -> {
-            int K = 100;
-
-            float temp = (float) endVector.length / M;
-
-            for (int i = tempValue.get(); i < tempValue.get() + temp; i++) {
-                if (i == endVector.length) break;
-                else {
+            switch (taskValue) {
+                case 0 -> endVector[i] = Math.pow(startVector[i], C);
+                case 1 -> {
                     for (int j = 0; j < K; j++) {
                         endVector[i] += Math.pow(startVector[i], C);
                     }
-
-                    /*for (int j = 0; j < i; j++) {
+                }
+                case 2 -> {
+                    for (int j = 0; j < i; j++) {
                         endVector[i] += Math.pow(startVector[i], C);
-                    }*/
-
-                    //endVector[i] = Math.pow(startVector[i], C);
+                    }
                 }
             }
+        }
+    }
 
-            tempValue.set(tempValue.get() + (int) Math.ceil(temp));
+    public static void parallelFunction(double[] startVector, int M, int C, int K, int taskValue) {
+        double[] endVector = new double[startVector.length];
+        AtomicInteger tempValue = new AtomicInteger();
+        Object lock = new Object();
+
+        Runnable runnable = () -> {
+            synchronized (lock) {
+                float temp = (float) endVector.length / M;
+                //System.out.println("Temp = " + temp);
+
+                for (int i = tempValue.get(); i < tempValue.get() + temp; i++) {
+                    if (i == endVector.length) break;
+                    else {
+                        switch (taskValue) {
+                            case 0 -> {
+                                endVector[i] = Math.pow(startVector[i], C);
+                                //System.out.println("EndVector[" + i + "] = " + endVector[i]);
+                            }
+                            case 1 -> {
+                                for (int j = 0; j < K; j++) {
+                                    endVector[i] += Math.pow(startVector[i], C);
+                                }
+                            }
+                            case 2 -> {
+                                for (int j = 0; j < i; j++) {
+                                    endVector[i] += Math.pow(startVector[i], C);
+                                }
+                            }
+                        }
+                    }
+                }
+                tempValue.set(tempValue.get() + (int) Math.ceil(temp));
+            }
         };
 
         Thread[] threads = new Thread[M];
 
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(runnable);
-            threads[i].start();
-            threads[i].join();
+        synchronized (lock) {
+            for (int i = 0; i < threads.length; i++) {
+                threads[i] = new Thread(runnable);
+                threads[i].start();
+            }
         }
-
-        return endVector;
     }
 }
