b38ce185b0b5f7962ea08e3c9a73a09fed07638a
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-10-26 12:00:30 +0400

    1

--- ParallelSystem_Practice2/src/Main.java
+++ ParallelSystem_Practice2/src/Main.java
@@ -6,51 +6,24 @@ public class Main {
         int N = 225;
         int[] startArray = createStartArray(N);
         int[] consistentArray = consistentAlgorithm(startArray, N);
-        long time1 = System.nanoTime() / 1000;
 
-        System.out.println("Последовательный алгоритм...");
+        System.out.println("Последовательный алгоритм:");
         for (int j : consistentArray) {
             System.out.print(j + " ");
         }
 
-        long time2 = System.nanoTime() / 1000 - time1;
-        System.out.println("\nВремя = " + time2 + " мкс");
-
-        System.out.println();
-
-        System.out.println("Параллельные алгоритмы...");
-
-        int[] modifiedArray1 = parallelAlgorithm(startArray, 2, 1);
-
-        for (int j : modifiedArray1) {
-            System.out.print(j + " ");
+        System.out.println("\n\nПараллельные алгоритмы:");
+        for (int i = 1; i <= 3; i++) {
+            long time1 = System.nanoTime() / 1000;
+            int[] modifiedArray1 = parallelAlgorithm(startArray, 2, i);
+            for (int j : modifiedArray1) {
+                System.out.print(j + " ");
+            }
+            long time2 = System.nanoTime() / 1000 - time1;
+            System.out.println("\nВремя = " + time2 + " мкс\n");
         }
 
-        long time3 = System.nanoTime() / 1000 - time1 - time2;
-        System.out.println("\nВремя = " + time3 + " мкс");
-
-        System.out.println();
-
-        int[] modifiedArray2 = parallelAlgorithm(startArray, 2, 2);
-
-        for (int j : modifiedArray2) {
-            System.out.print(j + " ");
-        }
-
-        long time4 = System.nanoTime() / 1000 - time1 - time2 - time3;
-        System.out.println("\nВремя = " + time4 + " мкс");
-
-        System.out.println();
-
-        int[] modifiedArray3 = parallelAlgorithm(startArray, 2, 3);
-
-        for (int j : modifiedArray3) {
-            System.out.print(j + " ");
-        }
-
-        long time5 = System.nanoTime() / 1000 - time1 - time2 - time3 - time4;
-        System.out.println("\nВремя = " + time5 + " мкс");
-
+        System.exit(0);
     }
 
     public static int[] createStartArray(int N) {
@@ -111,21 +84,7 @@ public class Main {
         atomicInteger1.set(floor - 2);
         atomicInteger2.set(0);
 
-        Runnable task = () -> {
-            int length = (basicArray.length) / M;
-            for (int i = atomicInteger.get(); i <= atomicInteger.get() + length; i++) {
-                if (i >= basicArray.length) break;
-                for (int j = floor - 2; j < startArray.length; j++) {
-                    if (modifiedArray[j] == 1) continue;
-                    if (startArray[j] % basicArray[i] == 0) {
-                        modifiedArray[j] = 1;
-                    } else modifiedArray[j] = 0;
-                }
-            }
-            atomicInteger.set(atomicInteger.get() + length);
-        };
-
-        Runnable task1 = () -> {
+        Runnable task1 = () -> { //Декомпозиция по данным
             int length = (startArray.length - (floor - 2)) / M;
             for (int i = atomicInteger1.get(); i <= atomicInteger1.get() + length; i++) {
                 if (i >= startArray.length) break;
@@ -139,7 +98,21 @@ public class Main {
             atomicInteger1.set(atomicInteger1.get() + length);
         };
 
-        Runnable task2 = () -> {
+        Runnable task2 = () -> { //Декомпозиция набора базовых простых чисел
+            int length = (basicArray.length) / M;
+            for (int i = atomicInteger.get(); i <= atomicInteger.get() + length; i++) {
+                if (i >= basicArray.length) break;
+                for (int j = floor - 2; j < startArray.length; j++) {
+                    if (modifiedArray[j] == 1) continue;
+                    if (startArray[j] % basicArray[i] == 0) {
+                        modifiedArray[j] = 1;
+                    } else modifiedArray[j] = 0;
+                }
+            }
+            atomicInteger.set(atomicInteger.get() + length);
+        };
+
+        Runnable task3 = () -> { //Последовательный перебор
             synchronized (lock) {
                 for (int i = atomicInteger2.get(); i <= atomicInteger2.get() + 1; i++) {
                     if (i >= basicArray.length) break;
@@ -158,19 +131,18 @@ public class Main {
 
         for (int i = 0; i < threads.length; i++) {
             if (value == 1) {
-                threads[i] = new Thread(task);
-                threads[i].start();
-                threads[i].join();
-            } else if (value == 2) {
                 threads[i] = new Thread(task1);
                 threads[i].start();
-                threads[i].join();
-            } else if (value == 3) {
+            } else if (value == 2) {
                 threads[i] = new Thread(task2);
                 threads[i].start();
+            } else if (value == 3) {
+                threads[i] = new Thread(task3);
+                threads[i].start();
                 if (atomicInteger2.get() == basicArray.length) break;
-                else if (i == threads.length - 1) i = -1;
+                else if (i == threads.length - 1) i = 0;
             }
+            threads[i].join();
         }
 
         for (int i = 0; i <= (floor - 2); i++) {
