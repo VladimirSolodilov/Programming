d197462373c4e11d38e675a30e519ebd6a137d6b
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-11-02 10:20:53 +0400

    1

--- BankingSystem/src/main/java/com/example/bankingsystem/BankingSystemApplication.java
+++ BankingSystem/src/main/java/com/example/bankingsystem/BankingSystemApplication.java
@@ -2,9 +2,15 @@ package com.example.bankingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BankingSystemApplication {
+
+    @GetMapping("/")
+    public String HelloWorld() {
+        return "Hello, world!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BankingSystemApplication.class, args);
