ac34ec02439be181b11d2d2ac5a65be5b6e49437
VladimirSolodilov <63406033+VladimirSolodilov@users.noreply.github.com>
2022-12-05 21:33:36 +0400

    Update PaymentRowMapper.java

--- BankingSystem/src/main/java/com/example/bankingsystem/data/payment/PaymentRowMapper.java
+++ BankingSystem/src/main/java/com/example/bankingsystem/data/payment/PaymentRowMapper.java
@@ -19,7 +19,7 @@ public class PaymentRowMapper implements RowMapper<Payment> {
 
         paymentModel.setPurpose(purpose);
         purpose.setPurposeName(rs.getString("PurposeName"));
-        
+
         return paymentModel;
     }
 }
